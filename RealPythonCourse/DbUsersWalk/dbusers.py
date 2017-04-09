83c0ced36830517ec32c623e4a5b986d7d6e9758
BorislavIliev <borislavtodorov1990@gmail.com>
2017-04-09 22:20:32 +0300

    db users latest

--- RealPythonCourse/DbUsersWalk/dbusers.py
+++ RealPythonCourse/DbUsersWalk/dbusers.py
@@ -4,9 +4,24 @@ import fnmatch
 import glob
 from os import walk
 import re
+import MySQLdb
+
+def mysql_connection(dbuser, dbname, dbpass):
+	db = MySQLdb.connect(host = 'localhost', user = dbuser, passwd = dbpass, db = dbname)
+
+# Check if connection was successful
+	if (db):
+    # Carry out normal procedure
+		#print "Connection successful"
+  		return 1
+	else:
+    # Terminate
+    		#print "Connection unsuccessful"
+		return 0
 
 for root, subFolders, files in os.walk('/home/borislav/'):
 	wp_files = []
+	wp_users = dict()
 	if 'wp-config.php' in files:
 		print(os.path.join(root, 'wp-config.php'))
 		wp_file_path = os.path.join(root, 'wp-config.php')
@@ -17,10 +32,29 @@ for root, subFolders, files in os.walk('/home/borislav/'):
 			for line in wp_file:
 				if 'define(\'DB_USER\'' in line:
 					new_line = line.strip('\n')
-					if(re.match('define.*DB_USER.*\'(.*)\'', new_line)):
-						m = re.match('define.*DB_USER.*\'(.*)\'', new_line)
-						print m.groups()[0]
-										
+                                        if(re.match('define.*DB_USER.*\'(.*)\'', new_line)):
+                                                m = re.match('define.*DB_USER.*\'(.*)\'', new_line)
+                                                dbuser = m.groups()[0]
+						print dbuser
+				elif 'define(\'DB_NAME\'' in line:
+                                        new_line = line.strip('\n')
+                                        if(re.match('define.*DB_NAME.*\'(.*)\'', new_line)):
+                                                m = re.match('define.*DB_NAME.*\'(.*)\'', new_line)
+                                                dbname = m.groups()[0]
+						print dbname
+				elif 'define(\'DB_PASSWORD\'' in line:
+                                        new_line = line.strip('\n')
+                                        if(re.match('define.*DB_PASSWORD.*\'(.*)\'', new_line)):
+                                                m = re.match('define.*DB_PASSWORD.*\'(.*)\'', new_line)
+                                                dbpass = m.groups()[0]
+						print dbpass
+#					print mysql_connection(dbuser, dbuser, dbpass)
+					conn_result = mysql_connection(dbuser, dbuser, dbpass)
+					if conn_result == 1:
+						print "Change"
+					else:
+						print "No change"
+
 #for root, subFolders, files in os.walk('/home/borislav/'):
 #	joomla_files = []
 #       	if 'configuration.php' in files:
